07564bfb410690eb5d74b241fd08e5582fb3472c
Randima <randima.cse@gmail.com>
2020-03-17 11:10:41 +0530

    Added product data sql

---
+++ resources/products.sql
@@ -0,0 +1,29 @@
+INSERT INTO public.product(name, "imgUrl", price)
+	VALUES ('Men''s Swearshirt', 'https://images-na.ssl-images-amazon.com/images/I/91u6De8BqgL._AC_UL200_SR200,200_.jpg', 200);
+INSERT INTO public.product(name, "imgUrl", price) VALUES 
+('High Waist Yoga Pants', 'https://images-na.ssl-images-amazon.com/images/I/71A5aQXiZRL._AC_UL200_SR200,200_.jpg', 200);
+
+INSERT INTO public.product(name, "imgUrl", price) VALUES 
+('Men''s T-Shirt', 'https://images-na.ssl-images-amazon.com/images/I/71FKlHTAOuL._AC_UL200_SR200,200_.jpg', 200);
+INSERT INTO public.product(name, "imgUrl", price) VALUES 
+('Men''s Boxer Briefs', 'https://images-na.ssl-images-amazon.com/images/I/81VukuLK%2BXL._AC_UL200_SR200,200_.jpg', 200);
+INSERT INTO public.product(name, "imgUrl", price) VALUES 
+('Men Leather Belt', 'https://images-na.ssl-images-amazon.com/images/I/81GoxQrbYuL._AC_UL200_SR200,200_.jpg', 200);
+INSERT INTO public.product(name, "imgUrl", price) VALUES 
+('Naturals Hand Sanitizer', 'https://images-na.ssl-images-amazon.com/images/I/51oJYvXEQVL._AC_UL200_SR200,200_.jpg', 200);
+INSERT INTO public.product(name, "imgUrl", price) VALUES 
+('Levi Women''s Skinny Jeans', 'https://images-na.ssl-images-amazon.com/images/I/81QiLd8WUYL._AC_UL200_SR200,200_.jpg', 200);
+INSERT INTO public.product(name, "imgUrl", price) VALUES 
+('Disposable Face Mask', 'https://images-na.ssl-images-amazon.com/images/I/71ERkRm83sL._AC_UL200_SR200,200_.jpg', 200);
+INSERT INTO public.product(name, "imgUrl", price) VALUES 
+('Men''s SHort Sleeve T-Shirt', 'https://images-na.ssl-images-amazon.com/images/I/91CcSmdYdRL._AC_UL200_SR200,200_.jpg', 200);
+INSERT INTO public.product(name, "imgUrl", price) VALUES 
+('Women''s Short Sleeve Dress', 'https://images-na.ssl-images-amazon.com/images/I/51RHNKQqMiL._AC_UL200_SR200,200_.jpg', 200);
+INSERT INTO public.product(name, "imgUrl", price) VALUES 
+('Women''s High Waist Leggings', 'https://images-na.ssl-images-amazon.com/images/I/41oA4cmBjxL._AC_UL200_SR200,200_.jpg', 200);
+INSERT INTO public.product(name, "imgUrl", price) VALUES 
+('Women''s High Waist Pattern Leggings', 'https://images-na.ssl-images-amazon.com/images/I/712OGQ%2BcZGL._AC_UL200_SR200,200_.jpg', 200);
+INSERT INTO public.product(name, "imgUrl", price) VALUES 
+('Women''s Walking Shoes', 'https://images-na.ssl-images-amazon.com/images/I/61Nh8BmRvoL._AC_UL200_SR200,200_.jpg', 200);
+INSERT INTO public.product(name, "imgUrl", price) VALUES 
+('Women''s High Waist Yoga Pants', 'https://images-na.ssl-images-amazon.com/images/I/71xVE%2BVijKL._AC_UL200_SR200,200_.jpg', 200);
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { RegisterModule } from './register/register.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeOrmConfig),
-    ProductsModule],
+    ProductsModule,
+    RegisterModule,
+    AuthModule],
   controllers: [],
   providers: [],
 })
